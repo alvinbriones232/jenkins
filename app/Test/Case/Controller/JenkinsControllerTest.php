4e8ad6fb4dcb30e6318a664f406c911d5fd43d12
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-19 14:14:07 +0800

    aa

--- app/Test/Case/Controller/JenkinsControllerTest.php
+++ app/Test/Case/Controller/JenkinsControllerTest.php
@@ -5,26 +5,8 @@ class JenkinsControllerTest extends ControllerTestCase {
 		'app.message'
 	);*/
 	public function testIndex() {
-		/*$act = $this->testAction('/jenkins/index');
-		if($act == true) {
-			$this->assertTrue($act);
-		} else {
-			$this->assertFalse($act);
-		}*/
-		$this->autoRender = false;
-		$this->Test = ClassRegistry::init('Test');
-		$data = array('Test' => array(
-			'first_name' => 'James Fairhurst',
-			'last_name' => 'asdasdsdasdas',
-			'date_created' => date('Y-m-d H:i:s'),
-			'date_modified' => date('Y-m-d H:i:s')
-		));
-
-		// Attempt to save
-		$result = $this->Test->save($data);
-		var_dump($result);
-		$this->assertTrue(TRUE);
-		
+		$act = $this->testAction('/jenkins/index');
+		$this->assertTrue($act);
 	}
 
 }
