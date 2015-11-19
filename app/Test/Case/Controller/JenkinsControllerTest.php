c37031beb65a591c9a6aeed5d320436876ba8d4f
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-19 13:37:27 +0800

    aa

--- app/Test/Case/Controller/JenkinsControllerTest.php
+++ app/Test/Case/Controller/JenkinsControllerTest.php
@@ -6,11 +6,7 @@ class JenkinsControllerTest extends ControllerTestCase {
 	);*/
 	public function testIndex() {
 		$act = $this->testAction('/jenkins/index');
-		if($act) {
-			$this->assertTrue($act);
-		} else {
-			$this->assertFalse($act);
-		}
+		var_dump($act);
 		
 	}
 
