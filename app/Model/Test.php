25db7d31300fbac4f6c998f606243f23298d8d69
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-19 13:13:59 +0800

    add model

---
+++ app/Model/Test.php
@@ -0,0 +1,23 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * MessageContent Model
+ *
+ * @property Message $Message
+ * @property From $From
+ */
+class Test extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array();
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+}
