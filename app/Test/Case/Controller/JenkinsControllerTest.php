5607bb59de54549e6eebc2d45e78e7359cc882bc
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-19 09:13:03 +0800

    tests

--- app/Test/Case/Controller/JenkinsControllerTest.php
+++ app/Test/Case/Controller/JenkinsControllerTest.php
@@ -3,9 +3,8 @@ App::uses('MessagesController', 'Controller');
 class JenkinsControllerTest extends ControllerTestCase {
 
 	public function testIndex() {
-		$a = $this->testAction('/');
 		$this->expectOutputString('bar');
-		print($a);
+        print 'test';
 	}
 
 }
